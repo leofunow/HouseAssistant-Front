0ff06c40e7485ce1586456bf4be1c9c531228a4d
Lev Shelopaev <leofunow@gmail.com>
2023-04-07 17:26:33 +0300

    ng-zorro init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,16 @@ import { FooterComponent } from './add-comp/footer/footer.component';
 import { IndexComponent } from './comp/index/index.component';
 import { LoginComponent } from './comp/login/login.component';
 import { CardComponent } from './comp/card/card.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { ru_RU } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
+registerLocaleData(ru);
 
 @NgModule({
   declarations: [
@@ -20,9 +30,15 @@ import { CardComponent } from './comp/card/card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NzButtonModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: ru_RU }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
