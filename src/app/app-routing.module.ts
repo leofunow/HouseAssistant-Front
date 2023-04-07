6f53cd1b156c2d8bbce092a48e412ecba12c6b4d
Lev Shelopaev <leofunow@gmail.com>
2023-04-07 17:20:52 +0300

    init routing& components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './comp/card/card.component';
+import { IndexComponent } from './comp/index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "", component: IndexComponent
+  },
+  {
+    path: "card", component: CardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './add-comp/header/header.component';
+import { FooterComponent } from './add-comp/footer/footer.component';
+import { IndexComponent } from './comp/index/index.component';
+import { LoginComponent } from './comp/login/login.component';
+import { CardComponent } from './comp/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    IndexComponent,
+    LoginComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
