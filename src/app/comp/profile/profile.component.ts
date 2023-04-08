c788e8443de57fbb41635181c61c0d26730898be
leofunow <leofunow@gmail.com>
2023-04-08 14:34:41 +0300

    query params for profile

--- src/app/comp/profile/profile.component.ts
+++ src/app/comp/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -9,5 +10,20 @@ export class ProfileComponent {
 
   selectedMenu: 'profile' | 'addCard' | 'cards' = 'profile';
 
+  constructor(private route: ActivatedRoute, private router: Router) {
+    
+    route.params.subscribe(params => {
+      if( params['menu'])
+      this.selectedMenu = params['menu'];
+    })
+  }
+
+  selectMenu(menu: 'profile' | 'addCard' | 'cards') {
+    this.selectedMenu = menu
+    this.router.navigate(
+      ['.'],
+      {relativeTo: this.route, queryParams: {menu: menu}}
+    )
+  }
 
 }
