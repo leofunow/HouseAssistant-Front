c3999c627afa625e223027b8594887d7f2927853
leofunow <leofunow@gmail.com>
2023-04-09 16:42:43 +0300

    mobile profile fix

--- src/app/add-comp/meetings/meetings.component.ts
+++ src/app/add-comp/meetings/meetings.component.ts
@@ -57,6 +57,53 @@ export class MeetingsComponent {
     docs: [
       
     ]
+  },{
+    _id: '12',
+    Date: new Date(),
+    objects:[
+      {
+        _id: "1212",
+        pictures: [],
+        field: "string",
+        address: "string",
+        type: "string",
+        status: "string",
+        area: 20,
+      } as ObjectShortInfo,
+      {
+        _id: "56455",
+        pictures: [],
+        field: "string",
+        address: "string",
+        type: "string",
+        status: "string",
+        area: 20,
+      } as ObjectShortInfo,
+    ],
+    users:[
+      {
+        _id: "asdasd",
+          name: "Ванек",
+          desc: "string",
+          email: "string",
+          contacts: ["string"],
+          picture: "https://eanews.ru/uploads/images/2020/september/16/okkupay-pedofilyay-e1381757936342-300x225.jpg",
+      },
+      {
+        _id: "asdasd",
+          name: "string",
+          desc: "string",
+          email: "string",
+          contacts: ["string"],
+          picture: "string",
+      },
+    ],
+    name: 'Название',
+    result: 'Результат',
+    status: 'Завершено',
+    docs: [
+      
+    ]
   },
 ]
 
